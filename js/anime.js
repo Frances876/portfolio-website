2ef6a1b9c0099ea3279f092c10ad7d7f4e8cf3c2
ohanyere chukwunonso frances <ohanyerechukwunonso@gemail.com>
2025-10-19 16:35:42 -0700

    Fixed

--- js/anime.js
+++ js/anime.js
@@ -27,30 +27,3 @@ animate(chars2, {
 });
 
 
-//  const { animate, splitText, stagger } = anime;
-
-// const { chars:chars1 } = splitText('.home-profession-1', {chars: true});
-// const { chars:chars2 } = splitText('.home-profession-2', {chars: true});
-
-// animate(chars1, {
-//   y: [
-//     { to: ['100%', '0%'] },
-//     { to: '-100%', delay: 4000, ease: 'in(3)' }
-//   ],
-//   duration: 900,
-//   ease: 'out(3)',
-//   delay: stagger(50),
-//   loop: true,
-// });
-// animate(chars2, {
-//   y: [
-//     { to: ['100%', '0%'] },
-//     { to: '-100%', delay: 4000, ease: 'in(3)' }
-//   ],
-//   duration: 900,
-//   ease: 'out(3)',
-//   delay: stagger(50),
-//   loop: true,
-// });
-
-
