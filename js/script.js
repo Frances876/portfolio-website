bfd39035757ac3356e2a4556d6d7d245d351a210
ohanyere chukwunonso frances <ohanyerechukwunonso@gemail.com>
2025-10-19 18:18:38 -0700

    Fixed

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Toggle navbar
 function init() {
     const toggleButton = document.querySelector('.navbar .mobile-menu-toggle');
     // console.log(toggleButton);
@@ -7,11 +8,21 @@ function init() {
     toggleButton.addEventListener('click',function () {
         mobileMenu.classList.toggle('active');
     })
-    
-    
+
 }
 
 document.addEventListener('DOMContentLoaded', init);
+
+//change navbar background on scroll
+document.addEventListener('scroll',function () {
+    const navbar = document.querySelector('.navbar');
+    // console.log(navbar);
+    if (window.scrollY > 0) {
+        navbar.classList.add('navbar-scroll');
+    } else {
+        navbar.classList.remove('navbar-scroll');
+    }
+})
 
 
 
